bd564c476caefdf48ede318587b8490404d21e72
Adam Stuborn <a.stuborn@gmail.com>
2023-12-19 20:46:11 +0100

    (fix) bugg with videoplayer, fix styling

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,7 +1,7 @@
 export const VideoPlayer = () => {
   return (
     <div>
-      <video controls muted>
+      <video controls muted poster="/assets/poster.png">
         <source src="/assets/timelapspainting.mp4" type="video/mp4" />
       </video>
     </div>
