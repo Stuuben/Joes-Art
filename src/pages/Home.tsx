c9e785cdda0cc236adf64d4d306fba8c811cdc52
Adam Stuborn <a.stuborn@gmail.com>
2023-12-28 11:20:31 +0100

    (ref) rearrange content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,20 @@ import "./Home.scss";
 
 import { NewsSlider } from "../components/NewsSlider";
 import { Events } from "../components/Events";
+import { VideoPlayer } from "../components/VideoPlayer";
 
 export const Home = () => {
   return (
     <div className="main-wrapper">
       <div className="content-wrapper">
         <h3 className="title">Josefin Stuborn</h3>
-        <p className="image-p">
+        {/*   <p className="image-p">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
           sint ipsam, exercitationem voluptates, nihil laborum magnam non
           corrupti tenetur id, ab inventore hic? Non quod iure, dolorum facere
           minima in.
-        </p>
+        </p> */}
+        <VideoPlayer></VideoPlayer>
         <NewsSlider></NewsSlider>
         <Events></Events>
         <div></div>
